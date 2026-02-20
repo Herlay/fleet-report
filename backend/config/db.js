b871191095d2a540488e093ed30d5fbf0b5938d9
Herlay <aledamilolasolomon@gmail.com>
2026-02-20 19:11:17 +0100

    Use Railway public URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,20 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    decimalNumbers: true,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
 const testConnection = async () => {
     try {
